be9f075bc5dcb29281ab5de7d6739180c4666ad5
Randy Abernethy <randy@rx-m.com>
2017-05-25 08:35:18 -0700

    added other tests to go

--- thrift/go-cli/client.go
+++ thrift/go-cli/client.go
@@ -10,11 +10,13 @@ import (
 )
 
 func main() {
+	//Parse command line
 	hostPtr := flag.String("host", "localhost", "hostname to connect to")
 	portPtr := flag.Int("port", 9090, "port to connect to")
 	actionPtr := flag.Int("action", 1, "tests to run")
 	flag.Parse()
 	addr := *hostPtr + ":" + strconv.Itoa(*portPtr)
+	//Setup Thrift I/O stack
 	transEP, err := thrift.NewTSocket(addr)
 	if err != nil {
 		fmt.Println("Error creating socket:", err)
@@ -25,16 +27,33 @@ func main() {
 	protoFac := thrift.NewTCompactProtocolFactory()
 	cli := OpenSourceProjects.NewProjectsClientFactory(trans, protoFac)
         fmt.Printf("[Client] Host %s, Port %d, Action %d\n", *hostPtr, *portPtr, *actionPtr)
-
+	//Connect to the server
 	if err := trans.Open(); err != nil {
 		fmt.Println("Error opening socket:", err)
 		return
 	}
 	defer trans.Close()
-
+	//Setup a mock project
+	d := OpenSourceProjects.Date{2007,1,10}
+	p := OpenSourceProjects.Project{"Thrift", "ASF", &d}
+	//Run the test
 	start := time.Now()
-	for i := 0; i < 1000000; i++ {
-		_, _ = cli.Get("Thrift")
+	switch *actionPtr {
+		case 1:
+			for i := 0; i < 1000000; i++ {
+				_, _ = cli.Get("Thrift")
+			}
+		case 2:
+			for i := 0; i < 1000000; i++ {
+				_, _ = cli.Create(&p)
+			}
+		case 3:
+			for i := 0; i < 1000000; i++ {
+				_, _ = cli.Create(&p)
+				_, _ = cli.Get("Thrift")
+			}
+		default:
+			fmt.Println("Invalid action, must be 1-3")
 	}
 	elap := time.Since(start)
 	fmt.Printf("Time to get() 1000000 times: %v\n", elap)
