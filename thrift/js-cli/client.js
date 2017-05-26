c4bbef03814c3bd3f3b01df3bad7e46fa99dd5f0
Randy Abernethy <randy@rx-m.com>
2017-05-26 11:52:14 -0700

    fixed js client

--- thrift/js-cli/client.js
+++ thrift/js-cli/client.js
@@ -12,44 +12,57 @@ program
   .option('-a, --action <n>', 'Action (must be 1-3)', parseInt, 1)
   .parse(process.argv);
 
+var proj = ProjectsTypes.Project({name: 'Thrift', host: 'ASF', inception: {year: 2007, month: 1, day: 10}})
+
 var connection = thrift.createConnection(program.host, program.port, {
    transport: thrift.TBufferedTransport,
    protocol: thrift.TCompactProtocol
 }).on('error', function(error) {
    console.log(error);
 }).on("connect", function() {
-   console.log("[Client] Host " + program.host + ", Port " + program.port + ", Action " + program.action);
    var i = 0;
    var client = thrift.createClient(Projects, connection);
-   //Get
-   var cbGet = function(error, result) {
-     i++;
-     if (i < 1000000) {
-       client.get("Thrift",cb); 
-     } else {
-       connection.end();
-     }
-   };
-   client.get("Thrift", cbGet);
-   //Create
-   var cbCreate = function(error, result) {
-     i++;
-     if (i < 1000000) {
-       client.create("Thrift",cb); 
-     } else {
-       connection.end();
-     }
-   };
-   client.get("Thrift", cbGet);
-   //Create/Get
-   var cbCreateGet = function(error, result) {
-     i++;
-     if (i < 1000000) {
-       client.get("Thrift",cb); 
-     } else {
-       connection.end();
-     }
-   };
-   client.get("Thrift", cbGet);
+   switch (program.action) {
+      case 1:   //Get
+         var cbGet = function(error, result) {
+            i++;
+            if (i < 1000000) {
+               client.get("Thrift", cbGet); 
+            } else {
+               connection.end();
+            }
+         };
+         client.get("Thrift", cbGet);
+         break;
+
+      case 2:   //Create
+         var cbCreate = function(error, result) {
+            i++;
+            if (i < 1000000) {
+               client.create(proj, cbCreate); 
+            } else {
+               connection.end();
+            }
+         };
+         client.create(proj, cbCreate);
+         break;
+
+      case 3:   //Create/Get
+         var cbCreateGetC = function(error, result) {
+               client.get("Thrift", cbCreateGetG); 
+         }
+         var cbCreateGetG = function(error, result) {
+            i++;
+            if (i < 1000000) {
+               client.create(proj, cbCreateGetC); 
+            } else {
+               connection.end();
+            }
+         };
+         client.create(proj, cbCreateGetC);
+         break;
+   }
 });
+
+console.log("[Client] Host " + program.host + ", Port " + program.port + ", Action " + program.action);
 
