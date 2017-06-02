151f3d8da3c5f729b5cd16327558268540a4158c
Aki Sukegawa <nsuke@apache.org>
2017-06-02 14:21:04 +0900

    Further restructure demo dir for live usage

---
+++ demo/v2/idl_v2/project_v2.thrift
@@ -0,0 +1,25 @@
+namespace py OpenSourceProjects
+
+struct Date {
+    1: i16 year
+    2: i16 month
+    3: i16 day
+} 
+
+
+struct Project {
+    1: string name       //Proper project name
+    2: string host       //Hosting/control foundation or company
+    3: Date inception    //Date project was open sourced
+    4: i16 commits       //Number of commits
+}
+
+struct CreateResult {
+    1: i16 code
+    2: string message
+}
+
+service Projects {
+    Project get(1: string name)
+    CreateResult create(1: Project proj)
+}
